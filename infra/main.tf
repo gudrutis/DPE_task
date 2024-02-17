56c53b74ac5416cdc81c4c3a71c583ee1a697a89
Zygimantas Matonis <zygimantas.matonis@cern.ch>
2024-02-17 21:44:10 +0200

    added test data

--- infra/main.tf
+++ infra/main.tf
@@ -1,19 +1,25 @@
-
-provider "aws" { 
-  region = "us-west-2"
-}
-
-resource "aws_s3_bucket" "data_lake" {
-  bucket = "zm-test-my-datalake-bucket-sakdhsakdhskd"
+resource "aws_s3_bucket" "lake_raw" {
+  bucket = "${var.project_name}_lake_raw"
   acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle {
-    prevent_destroy = true
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 }
 
+resource "aws_s3_object" "debt" {
+  bucket = aws_s3_bucket.lake_raw.id
+  key    = "debt"
+  acl    = "private"
+  source = "../data/municipal_debt.csv"
+}
 
+resource "aws_s3_object" "expenses" {
+  bucket = aws_s3_bucket.lake_raw.id
+  key    = "expenses"
+  acl    = "private"
+  source = "../data/Vilnius_council_expenses.csv"
+}
